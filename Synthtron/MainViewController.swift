11206a60a0ba7232d5eda5fcbf08157cf1c1a805
Kyle Stewart <kylestew@gmail.com>
2017-06-02 16:49:49 -0700

    Keyboard as buttons, going to ditch this idea

--- Synthtron/MainViewController.swift
+++ Synthtron/MainViewController.swift
@@ -5,14 +5,21 @@ class MainViewController: UIViewController {
     
     let whiteKeyNormalColor = UIColor(rgb: 0xFFFFFF)
     let whiteKeyDownColor = UIColor(rgb: 0xDDDDDD)
+    let blackKeyNormalColor = UIColor(rgb: 0x232425)
+    let blackKeyDownColor = UIColor(rgb: 0x434445)
+    
+    let oscillator = AKOscillator()
+    
+    let baseNoteMIDI: MIDINoteNumber = 36 // C3
+    var currentMIDINote: MIDINoteNumber = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let oscillator = AKOscillator()
-        oscillator.amplitude = 0.1
         AudioKit.output = oscillator
         AudioKit.start()
+        
+        oscillator.amplitude = 0.0
         oscillator.start()
     }
 
@@ -20,27 +27,55 @@ class MainViewController: UIViewController {
     
     @IBAction func whiteKeyDown(_ sender: UIButton) {
         sender.backgroundColor = whiteKeyDownColor
+        keyOn(sender.tag)
     }
     
     @IBAction func whiteKeyUp(_ sender: UIButton) {
         sender.backgroundColor = whiteKeyNormalColor
+        keyOff(sender.tag)
+    }
+    
+    @IBAction func blackKeyDown(_ sender: UIButton) {
+        sender.backgroundColor = blackKeyDownColor
+        keyOn(sender.tag)
+    }
+    
+    @IBAction func blackKeyUp(_ sender: UIButton) {
+        sender.backgroundColor = blackKeyNormalColor
+        keyOff(sender.tag)
     }
     
     // MARK: - Play Some Notes
+    
+    // map key tags to note numbers
+    
     func keyOn(_ keyNumber: Int) {
-        
+        print(keyNumber)
+        noteOn(baseNoteMIDI + MIDINoteNumber(keyNumber))
     }
     
     func keyOff(_ keyNumber: Int) {
-        
+        noteOff(baseNoteMIDI + MIDINoteNumber(keyNumber))
     }
     
-    func noteOn(_ noteNumber: Int) {
+    func noteOn(_ note: MIDINoteNumber) {
         
+        currentMIDINote = note
+        // start from the correct note if amplitude is zero
+        if oscillator.amplitude == 0 {
+            oscillator.rampTime = 0
+        }
+        oscillator.frequency = note.midiNoteToFrequency()
+
+        // Still use rampTime for volume
+        oscillator.amplitude = 0.2
+        oscillator.play()
     }
     
-    func noteOff(_ noteNumber: Int) {
-        
+    func noteOff(_ note: MIDINoteNumber) {
+        if currentMIDINote == note {
+            oscillator.amplitude = 0
+        }
     }
     
 }
